88b63e8563e642d62e95440790d68c70590e3976
Victor Cheng <weitungstyle@gmail.com>
2022-10-21 15:15:40 +0800

    feat: connect to mongoDB

---
+++ config/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongoDB error!')
+})
+
+db.once('open', () => {
+  console.log('mongoDB connected!')
+})
